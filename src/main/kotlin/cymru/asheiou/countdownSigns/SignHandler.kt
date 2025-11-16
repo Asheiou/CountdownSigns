633496a556a2c22cfb1c156778aff987d3290d90
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-11-16 01:21:59 +0000

    unused imports

--- src/main/kotlin/cymru/asheiou/countdownSigns/SignHandler.kt
+++ src/main/kotlin/cymru/asheiou/countdownSigns/SignHandler.kt
@@ -1,93 +1,91 @@
-  package cymru.asheiou.countdownSigns
+package cymru.asheiou.countdownSigns
 
-  import kotlinx.serialization.json.Json
-  import org.bukkit.Bukkit
-  import org.bukkit.block.Block
-  import org.bukkit.block.Sign
-  import org.bukkit.block.sign.Side
-  import org.bukkit.scheduler.BukkitRunnable
-  import java.io.File
-  import kotlin.time.Duration
-  import kotlin.time.Duration.Companion.days
-  import kotlin.time.Duration.Companion.hours
-  import kotlin.time.Duration.Companion.milliseconds
-  import kotlin.time.Duration.Companion.minutes
+import kotlinx.serialization.json.Json
+import org.bukkit.Bukkit
+import org.bukkit.block.Block
+import org.bukkit.block.Sign
+import org.bukkit.block.sign.Side
+import org.bukkit.scheduler.BukkitRunnable
+import java.io.File
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 
 
-  class SignHandler(private val cs: CountdownSigns) : BukkitRunnable() {
-    val signs: MutableMap<Block, Long> = mutableMapOf()
-    val path = cs.dataFolder.toString() + File.separator + "signs.json"
-    val file = File(path)
+class SignHandler(private val cs: CountdownSigns) : BukkitRunnable() {
+  val signs: MutableMap<Block, Long> = mutableMapOf()
+  val path = cs.dataFolder.toString() + File.separator + "signs.json"
+  val file = File(path)
 
-    fun init() {
-      if (!file.exists()) {
-        cs.saveResource("signs.json", true)
-        return
-      }
-      signs.clear()
-      val signsJson = Json.decodeFromString<MutableList<SignData>>(file.readText())
-      signsJson.forEach {
-        val world = Bukkit.getServer().getWorld(it.world) ?: return@forEach
-        val block = world.getBlockAt(it.x, it.y, it.z)
-        signs.put(block, it.expiry)
-      }
+  fun init() {
+    if (!file.exists()) {
+      cs.saveResource("signs.json", true)
+      return
     }
-
-    override fun run() {
-      signs.forEach { block, expiry ->
-        if (!block.chunk.isLoaded) return@forEach
-        if (block.state !is Sign) {
-          cs.logger.fine("Block at ${block.x}, ${block.y}, ${block.z} is not a sign!")
-          return@forEach
-        }
-        val sign = block.state as Sign
-        val line = cs.config.getInt("line")
-        val format = cs.config.getString("format")!!
-        val formatted = formatDiff(expiry, format)
-
-        listOf(sign.getSide(Side.FRONT), sign.getSide(Side.BACK)).forEach { side ->
-          side.line(line, MessageSender.miniMessage.deserialize(formatted))
-        }
-
-        sign.update()
-      }
-    }
-
-    override fun cancel() {
-      saveAll()
-    }
-
-    fun saveAll() {
-      val signList = mutableListOf<SignData>()
-      signs.forEach { (block, expiry) ->
-        signList.add(SignData.fromBlock(block, expiry))
-      }
-      val signsJson = Json.encodeToString(signList)
-      file.writeText(signsJson)
-    }
-
-    companion object {
-      fun formatDiff(expiry: Long, format: String): String {
-        val now = System.currentTimeMillis()
-        var diff: Duration = (expiry - now).milliseconds
-
-
-        if (diff.isNegative()) diff = Duration.ZERO
-
-        var totalMinutes = diff.inWholeMinutes
-        val leftover = diff - totalMinutes.minutes
-        if (leftover.inWholeMilliseconds > 0) {
-          totalMinutes += 1
-        }
-
-        val days = totalMinutes / (60 * 24)
-        val hours = (totalMinutes % (60 * 24)) / 60
-        val minutes = totalMinutes % 60
-
-        return format
-          .replace("{d}", days.toString())
-          .replace("{h}", hours.toString())
-          .replace("{m}", minutes.toString())
-      }
+    signs.clear()
+    val signsJson = Json.decodeFromString<MutableList<SignData>>(file.readText())
+    signsJson.forEach {
+      val world = Bukkit.getServer().getWorld(it.world) ?: return@forEach
+      val block = world.getBlockAt(it.x, it.y, it.z)
+      signs.put(block, it.expiry)
     }
   }
+
+  override fun run() {
+    signs.forEach { block, expiry ->
+      if (!block.chunk.isLoaded) return@forEach
+      if (block.state !is Sign) {
+        cs.logger.fine("Block at ${block.x}, ${block.y}, ${block.z} is not a sign!")
+        return@forEach
+      }
+      val sign = block.state as Sign
+      val line = cs.config.getInt("line")
+      val format = cs.config.getString("format")!!
+      val formatted = formatDiff(expiry, format)
+
+      listOf(sign.getSide(Side.FRONT), sign.getSide(Side.BACK)).forEach { side ->
+        side.line(line, MessageSender.miniMessage.deserialize(formatted))
+      }
+
+      sign.update()
+    }
+  }
+
+  override fun cancel() {
+    saveAll()
+  }
+
+  fun saveAll() {
+    val signList = mutableListOf<SignData>()
+    signs.forEach { (block, expiry) ->
+      signList.add(SignData.fromBlock(block, expiry))
+    }
+    val signsJson = Json.encodeToString(signList)
+    file.writeText(signsJson)
+  }
+
+  companion object {
+    fun formatDiff(expiry: Long, format: String): String {
+      val now = System.currentTimeMillis()
+      var diff: Duration = (expiry - now).milliseconds
+
+
+      if (diff.isNegative()) diff = Duration.ZERO
+
+      var totalMinutes = diff.inWholeMinutes
+      val leftover = diff - totalMinutes.minutes
+      if (leftover.inWholeMilliseconds > 0) {
+        totalMinutes += 1
+      }
+
+      val days = totalMinutes / (60 * 24)
+      val hours = (totalMinutes % (60 * 24)) / 60
+      val minutes = totalMinutes % 60
+
+      return format
+        .replace("{d}", days.toString())
+        .replace("{h}", hours.toString())
+        .replace("{m}", minutes.toString())
+    }
+  }
+}
